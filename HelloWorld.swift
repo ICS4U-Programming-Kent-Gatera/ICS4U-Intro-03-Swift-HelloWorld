6c123af3a50aeb3aaf740f0df40e2491cf39698f
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-02-12 16:01:44 +0000

    Hello World

---
+++ HelloWorld.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Kent G
+//  Created on Year-Month-Day
+//  Version 1.0
+//  Copyright (c) Year Kent G. All rights reserved.
+//
+//  The program says Hello Spencer and asks how user is doing.
+
+print("Hello Spencer")
+print("How are you doing?")
